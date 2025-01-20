13c9235b2d12ff91234aa77b059a9c06efc5e049
lee-cheolhee <cheolhee.lee@farmily.ai>
2025-01-20 10:53:57 +0900

    ros 버전 별 브랜치 분리

--- ros-noetic/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PROJECT_NAME="rdv"
-
-REPO_NAME=ros
-TAG=noetic-desktop
-
-docker build -t $PROJECT_NAME/$REPO_NAME:$TAG -f ./Dockerfile .
-
--- ros2-humble/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PROJECT_NAME="rdv"
-
-REPO_NAME=ros2
-TAG=humble-desktop
-
-docker build -t $PROJECT_NAME/$REPO_NAME:$TAG -f ./Dockerfile .
-
