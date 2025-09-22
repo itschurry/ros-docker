f3b30a6645a70ecbdfb8290cbb291fbad9593031
lee-cheolhee <cheolhee.lee@farmily.ai>
2025-09-22 17:35:06 +0900

    buildx 빌드로 변경

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ REPO_NAME="ghcr.io/lee-cheolhee"
 PROJECT_NAME="ros-docker"
 TAG=$(git branch --show-current)
 
-docker build -t $REPO_NAME/$PROJECT_NAME:$TAG -f ./Dockerfile .
-# docker buildx build --platform linux/amd64,linux/arm64 -t $REPO_NAME/$PROJECT_NAME:$TAG -f ./Dockerfile . --push
+# docker build -t $REPO_NAME/$PROJECT_NAME:$TAG -f ./Dockerfile .
+docker buildx build --platform linux/amd64,linux/arm64 -t $REPO_NAME/$PROJECT_NAME:$TAG -f ./Dockerfile . --push
