468d954822d28cef5b4204ca41b0a4e816ec3ff0
itschurry <cheolhee75@icloud.com>
2025-11-25 10:29:58 +0900

    update

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-REPO_NAME="ghcr.io/rendezvue"
-PROJECT_NAME="ros_docker"
+REPO_NAME="ghcr.io/itschurry"
+PROJECT_NAME="ros"
 TAG=$(git branch --show-current)
 
-# docker build -t $REPO_NAME/$PROJECT_NAME:$TAG -f ./Dockerfile .
-docker buildx build --platform linux/amd64,linux/arm64 -t $REPO_NAME/$PROJECT_NAME:$TAG -f ./Dockerfile . --push
+docker build -t $REPO_NAME/$PROJECT_NAME:$TAG -f ./Dockerfile .
+# docker buildx build --platform linux/amd64,linux/arm64 -t $REPO_NAME/$PROJECT_NAME:$TAG -f ./Dockerfile . --push
