cf61f699e7cf39eadae6876c1bf6c5240de0597f
itschurry <cheolhee@icloud.com>
2025-12-01 14:03:17 +0900

    update

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-REPO_NAME="ghcr.io/lee-cheolhee"
-PROJECT_NAME="ros-docker"
+REPO_NAME="ghcr.io/itschurry"
+PROJECT_NAME="ros"
 TAG=$(git branch --show-current)
 
-docker buildx build --platform linux/amd64,linux/arm64 -t $REPO_NAME/$PROJECT_NAME:$TAG -f ./Dockerfile . --push
+docker build -t $REPO_NAME/$PROJECT_NAME:$TAG -f ./Dockerfile .
+# docker buildx build --platform linux/amd64,linux/arm64 -t $REPO_NAME/$PROJECT_NAME:$TAG -f ./Dockerfile . --push
 # --progrees plain
 
